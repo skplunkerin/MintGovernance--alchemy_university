8829cc02988ef63ca882f805273c2f9e0b9822af
skplunkerin <skplunkerin@pm.me>
2023-02-11 18:20:48 -0700

    Finish setting up project
    * Add `.DS_Store` to `.gitignore`
    
    * Update `MyGovernor.sol` voting period to 1 day
    
    * Add Goerli network to `hardhat.config.js`
    
    * Install `dotenv` package and add `.env-sample`
    
    * Add scripts to `package.json`:
      - `dev` - to run local Hardhat node
      - `compile` - to compile contracts
      - `deploy_local` - to deploy on localhost network
      - `deploy_goerli` - to deploy on Goerli network
      - `verify_goerli` - to verify deployed contracts on Etherscan
      - `test` - to run Hardhat tests
    
    * Fix `deploy.js` script owner variable to come from
      `ethers.getSigners()`
    
    * Add helpful comments to explain how things are working in `deploy.js`
      script and `MyGovernor.js` test files
    
    * Add `.prettierrc` to match 4 space tab width of original project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,16 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const [owner] = await ethers.getSigners();
   const transactionCount = await owner.getTransactionCount();
 
   // gets the address of the token before it is deployed
+  // NOTE: this is possible because contract addresses are deterministic; they
+  //       are the keccak hash of the address deploying the contract and the
+  //       nonce for that particular address.
   const futureAddress = ethers.utils.getContractAddress({
     from: owner.address,
-    nonce: transactionCount + 1
+    nonce: transactionCount + 1,
   });
 
   const MyGovernor = await ethers.getContractFactory("MyGovernor");
@@ -15,10 +19,8 @@ async function main() {
   const MyToken = await ethers.getContractFactory("MyToken");
   const token = await MyToken.deploy(governor.address);
 
-  console.log(
-    `Governor deployed to ${governor.address}`,
-    `Token deployed to ${token.address}`
-  );
+  console.log(`Governor deployed to:\t ${governor.address}`);
+  console.log(`Token deployed to:\t ${token.address}`);
 }
 
 main().catch((error) => {
--- test/MyGovernor.js
+++ test/MyGovernor.js
@@ -8,11 +8,11 @@ describe("MyGovernor", function () {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const transactionCount = await owner.getTransactionCount();
-  
+
     // gets the address of the token before it is deployed
     const futureAddress = ethers.utils.getContractAddress({
       from: owner.address,
-      nonce: transactionCount + 1
+      nonce: transactionCount + 1,
     });
 
     const MyGovernor = await ethers.getContractFactory("MyGovernor");
@@ -38,58 +38,100 @@ describe("MyGovernor", function () {
       const deployValues = await deployFixture();
       const { governor, token, owner } = deployValues;
 
+      // NOTE: the 1st signer is connected to all `ContractFactory` and
+      //       `Contract` instances by default; using `connect()` is only
+      //       required when a different signer is needed:
+      //
+      //   > "By default, `ContractFactory` and `Contract` instances are
+      //   > connected to the first signer.
+      //   > [...]
+      //   > If you need to test your code by sending a transaction from an
+      //   > account (or `Signer` in `ethers.js` terminology) other than the
+      //   > default one, you can use the `connect()` method."
+      //   >
+      //   > Source: https://hardhat.org/tutorial/testing-contracts
       const tx = await governor.propose(
         [token.address],
         [0],
-        [token.interface.encodeFunctionData("mint", [owner.address, parseEther("25000")])],
+        // NOTE: parseEther() can be used because this erc20 token uses 18
+        //       decimals just like ether.
+        [
+          token.interface.encodeFunctionData("mint", [
+            owner.address,
+            parseEther("25000"),
+          ]),
+        ],
         "Give the owner more tokens!"
       );
       const receipt = await tx.wait();
-      const event = receipt.events.find(x => x.event === 'ProposalCreated');
+      const event = receipt.events.find((x) => x.event === "ProposalCreated");
       const { proposalId } = event.args;
 
       // wait for the 1 block voting delay
       await hre.network.provider.send("evm_mine");
-      
-      return { ...deployValues, proposalId } 
+
+      return { ...deployValues, proposalId };
     }
-    
+
     it("should set the initial state of the proposal", async () => {
       const { governor, proposalId } = await loadFixture(afterProposingFixture);
-      
+
       const state = await governor.state(proposalId);
       assert.equal(state, 0);
     });
-    
+
     describe("after voting", () => {
       async function afterVotingFixture() {
         const proposingValues = await afterProposingFixture();
         const { governor, proposalId } = proposingValues;
-        
-        const tx = await governor.castVote(proposalId, 1);      
+
+        // NOTES:
+        // 1. the owner (the 1st signer) is connected to this instance by
+        //    default.
+        //
+        // 2. The owner has a weight of 10000 tokens which will be enough for
+        //    the vote to be successful.
+        //
+        // 3. Normally the next step would be to queue the proposal in the
+        //    `Timelock` to wait for some period before execution, but that has
+        //    been excluded from the project to simplify things (go ahead and
+        //    execute this after the voting period has ended).
+        const tx = await governor.castVote(proposalId, 1);
         const receipt = await tx.wait();
-        const voteCastEvent = receipt.events.find(x => x.event === 'VoteCast');
-        
+        const voteCastEvent = receipt.events.find(
+          (x) => x.event === "VoteCast"
+        );
+
         // wait for the 1 block voting period
         await hre.network.provider.send("evm_mine");
 
-        return { ...proposingValues, voteCastEvent }
+        return { ...proposingValues, voteCastEvent };
       }
 
       it("should have set the vote", async () => {
         const { voteCastEvent, owner } = await loadFixture(afterVotingFixture);
 
         assert.equal(voteCastEvent.args.voter, owner.address);
-        assert.equal(voteCastEvent.args.weight.toString(), parseEther("10000").toString());
+        assert.equal(
+          voteCastEvent.args.weight.toString(),
+          parseEther("10000").toString()
+        );
       });
 
       it("should allow executing the proposal", async () => {
-        const { governor, token, owner } = await loadFixture(afterVotingFixture);
+        const { governor, token, owner } = await loadFixture(
+          afterVotingFixture
+        );
 
         await governor.execute(
           [token.address],
           [0],
-          [token.interface.encodeFunctionData("mint", [owner.address, parseEther("25000")])],
+          [
+            token.interface.encodeFunctionData("mint", [
+              owner.address,
+              parseEther("25000"),
+            ]),
+          ],
           keccak256(toUtf8Bytes("Give the owner more tokens!"))
         );
 
